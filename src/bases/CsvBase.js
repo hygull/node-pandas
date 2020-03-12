5d8eb66bf2f287b2720e8a5056a8b324c129a841
Martina <martina.soluncevska96@gmail.com>
2020-03-12 17:30:00 +0100

    fixed issue with undefined variable

--- src/bases/CsvBase.js
+++ src/bases/CsvBase.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 const path = require('path')
-const messages  = require('../messages/messages')
+const messages = require('../messages/messages')
 
 // https://javascript.info/mixins
 const CsvBase = {
-    toCsv(outCsvPath=null) {
+    toCsv(outCsvPath = null) {
         let pathDetails
 
-        if(outCsvPath) {
+        if (outCsvPath) {
             pathDetails = path.parse(outCsvPath).dir
         } else {
             pathDetails = null
@@ -22,25 +22,25 @@ const CsvBase = {
             }
         */
 
-        if(pathDetails && fs.existsSync(pathDetails)) {
+        if (pathDetails && fs.existsSync(pathDetails)) {
             let csvText = ''
-            
+
             csvText += this.columns.join(',') + '\n'
 
-            for(let r=0; r < this.rows; ++r) {
-                for(let c=0; c < this.cols; ++c) {
-                    csvText += this.data[r][columns[c]] + ','
+            for (let r = 0; r < this.rows; ++r) {
+                for (let c = 0; c < this.cols; ++c) {
+                    csvText += this.data[r][this.columns[c]] + ','
                 }
-                csvText = csvText.trim().slice(0, -1)  + '\n' // Remove , from end of last line
+                csvText = csvText.trim().slice(0, -1) + '\n' // Remove , from end of last line
             }
 
             csvText = csvText.trim()
 
             fs.writeFile(outCsvPath, csvText, (error) => {
-                if(error) {
+                if (error) {
                     messages.error(`${error}`)
                 } else {
-                    if(this.out) {
+                    if (this.out) {
                         console.log(`CSV file is successfully created at ${outCsvPath}`)
                     }
                 }
