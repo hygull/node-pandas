f1deb2f102cfb3498155caee7f607eaaddb4e115
biiiim <120476871+biiiim@users.noreply.github.com>
2024-03-20 11:26:05 +0000

    Update utils.js
    fix the failed require to datatype
    datatype file is all lowercase

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-const dataType = require('./dataType') // require('dataType') => Error: Cannot find module 'dataType'
+const dataType = require(__dirname+'/datatype') // require('dataType') => Error: Cannot find module 'dataType'
 const getTransformedDataList = require('./getTransformedDataList')
 const getIndicesColumns = require('./getIndicesColumns')
 const excludingColumns = require("./excludingColumns")
